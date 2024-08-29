dddf783be493e191fd010bda9ae3a0c8fc1228a0
Jaco Du Toit <jdt@neuronical.com>
2024-08-29 17:40:52 +0200

    add player movement

--- main.py
+++ main.py
@@ -18,19 +18,27 @@ def main():
     y = SCREEN_HEIGHT//2
     player = Player(x, y)
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    updatable.add(player)
+    drawable.add(player)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        
+       
         screen.fill(0)
-        player.draw(screen)
+        for d in drawable:
+            d.draw(screen)
 
         pygame.display.flip()
                 
         dt = clock.tick(60) / 1000
-        player.update(dt)
+
+        for u in updatable:
+            u.update(dt)
 
 if __name__ == "__main__":
     main()
